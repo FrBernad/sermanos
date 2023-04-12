ef58f2b153804a16f941a809843a463d87bb0200
Agustin Manfredi <imanfredi@itba.edu.ar>
2023-04-12 14:55:10 -0300

    feat: add shadows token

--- lib/config/theme/app_shadows.dart
+++ lib/config/theme/app_shadows.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-import 'app_colors.dart';
+import '../../design_system/tokens/app_colors.dart';
 
 class AppShadows {
   AppShadows._();
 
-  static const shadow1 = BoxShadow(
-    color: AppColors.neutral10,
-  );
-  static const shadow2 = BoxShadow();
-  static const shadow3 = BoxShadow();
+  static List<BoxShadow> shadow1 = [
+    BoxShadow(
+      color: Colors.black.withOpacity(0.3),
+      offset: const Offset(0, 1), // changes position of shadow
+      blurRadius: 2,
+    ),
+    BoxShadow(
+      color: Colors.black.withOpacity(0.15),
+      offset: const Offset(0, 1), // changes position of shadow
+      blurRadius: 3,
+      spreadRadius: 1,
+    ),
+  ];
+  static List<BoxShadow> shadow2 = [
+    BoxShadow(
+      color: Colors.black.withOpacity(0.3),
+      offset: const Offset(0, 1), // changes position of shadow
+      blurRadius: 2,
+    ),
+    BoxShadow(
+      color: Colors.black.withOpacity(0.15),
+      offset: const Offset(0, 2), // changes position of shadow
+      blurRadius: 6,
+      spreadRadius: 2,
+    ),
+  ];
+  static List<BoxShadow> shadow3 = [
+    BoxShadow(
+      color: Colors.black.withOpacity(0.15),
+      offset: const Offset(0, 8), // changes position of shadow
+      blurRadius: 12,
+      spreadRadius: 6,
+    ),
+    BoxShadow(
+      color: Colors.black.withOpacity(0.3),
+      offset: const Offset(0, 4), // changes position of shadow
+      blurRadius: 4,
+    ),
+  ];
 }
