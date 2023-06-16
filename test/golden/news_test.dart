c30b0091ed7944c16ce34590cb48a4e760863f22
Agustin Manfredi <imanfredi@itba.edu.ar>
2023-06-16 20:21:27 -0300

    fix: delete unused dependency

--- test/golden/news_test.dart
+++ test/golden/news_test.dart
@@ -2,40 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nock/nock.dart';
 import 'package:sermanos/features/news/application/controllers/news_search_controller.dart';
 import 'package:sermanos/features/news/presentation/screens/news_screen.dart';
 
 import 'news_search_controller_mock.dart';
 
 void main() {
-  setUpAll(() {
-    nock.init();
-  });
-
-  setUp(() {
-    nock.cleanAll();
-    const MethodChannel('plugins.flutter.io/path_provider')
-        .setMethodCallHandler(
-      (MethodCall methodCall) async {
-        if (methodCall.method == 'getApplicationSupportDirectory') {
-          return <String, dynamic>{}; // set initial values here if desired
-        } else if (methodCall.method == 'getTemporaryDirectory') {
-          return <String, dynamic>{}; // set initial values here if desired
-        }
-
-        return null;
-      },
-    );
-  });
   testWidgets(
     'News Screen Golden Test',
     (WidgetTester tester) async {
-      nock('').get('').reply(
-            200,
-            "result",
-          );
-
       await tester.pumpWidget(
         Localizations(
           locale: const Locale('en', 'US'),
--- test/golden/postulate_details_test.dart
+++ test/golden/postulate_details_test.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nock/nock.dart';
 import 'package:sermanos/features/postulate/application/controllers/get_volunteering_by_id_controller.dart';
 import 'package:sermanos/features/postulate/presentation/screens/postulate_detail_screen.dart';
 
-import '../../.fvm/flutter_sdk/packages/flutter/lib/src/services/platform_channel.dart';
 import 'mocks/providers/get_volunteering_by_id_controller_mock.dart';
 import 'mocks/volunteering_id_mock.dart';
 
